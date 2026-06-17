02be56bb9bef1aa94475b99fc09552d2ec43e226
dvoojt-art <dvo_ojt@callboxinc.com>
2026-06-17 15:19:56 -0400

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -187,7 +187,7 @@ if (!authorized) {
               </div>
               <span className="font-headline font-bold text-3xl tracking-tight text-white">FastDocs</span>
             </div>
-            <span className="text-[10px] font-bold uppercase tracking-[0.2em] text-primary mt-1">Callb<span className="relative inline-block">o<ChevronUp className="absolute -top-[0.2em] left-1/2 -translate-x-1/2 h-[0.5em] w-[0.5em] text-primary" strokeWidth={4} /></span>x Inc. Davao</span>
+           <span className="text-[8px] font-bold uppercase tracking-[0.2em] text-primary ml-10 -mt-0.5 group-data-[collapsible=icon]:hidden">Callb<span className="relative inline-block">o<ChevronUp className="absolute -top-[0.2em] left-1/2 -translate-x-1/2 h-[0.5em] w-[0.5em] text-primary" strokeWidth={4} /></span>x Inc. Davao</span>
           </Link>
         </div>
 
